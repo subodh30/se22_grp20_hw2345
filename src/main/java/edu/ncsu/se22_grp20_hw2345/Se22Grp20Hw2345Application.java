b65755dc985ea5d22567430d3e7b9c14274026e2
ameyagv <ameyav1234@gmail.com>
2022-08-31 12:53:52 -0400

    creating a springboot project

---
+++ src/main/java/edu/ncsu/se22_grp20_hw2345/Se22Grp20Hw2345Application.java
@@ -0,0 +1,13 @@
+package edu.ncsu.se22_grp20_hw2345;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se22Grp20Hw2345Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Se22Grp20Hw2345Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ncsu/se22_grp20_hw2345/Se22Grp20Hw2345ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ncsu.se22_grp20_hw2345;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se22Grp20Hw2345ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
