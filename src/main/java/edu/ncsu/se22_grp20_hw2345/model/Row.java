cb6fb81594ee81087e64623a807973d43f8db3fe
unknown <yashsonar213@gmail.com>
2022-09-13 01:26:14 -0400

    Add Row class

--- src/main/java/edu/ncsu/se22_grp20_hw2345/model/Row.java
+++ src/main/java/edu/ncsu/se22_grp20_hw2345/model/Row.java
@@ -3,6 +3,8 @@ package edu.ncsu.se22_grp20_hw2345.model;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -12,4 +14,17 @@ public class Row {
     List<String> cells;
     boolean isEval;
     List<String> cooked;
+
+    public Row(List<String> t) {
+        this.cells = t;
+        this.isEval = false;
+        this.cooked = clone_list(t);
+    }
+    public static List<String> clone_list(List<String> record){
+        List<String> temp_element = new ArrayList<String>();
+        for(String element:record){
+            temp_element.add(element);
+        }
+        return temp_element;
+    }
 }
