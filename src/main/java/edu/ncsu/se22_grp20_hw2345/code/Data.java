415a29b39aad276847e846bb58352f8d60b09b0e
Kunal Patil <kunalpatil1810@gmail.com>
2022-09-26 16:50:32 -0400

    Added add(), round(), and stats() function descriptions in Data.java

--- src/main/java/edu/ncsu/se22_grp20_hw2345/code/Data.java
+++ src/main/java/edu/ncsu/se22_grp20_hw2345/code/Data.java
@@ -4,6 +4,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Data Class keeps a list of rows and their sumaries in columns
+ */
+
 @lombok.Data
 public class Data {
     Columns columns;
@@ -18,6 +22,11 @@ public class Data {
         }
     }
 
+    /**
+     *
+     *     This function fetches a row and adds its cell values to the columns.
+     */
+
     public void add(Row row) {
         if (null == this.getColumns()) {
             setColumns(new Columns(row.getCells()));
@@ -26,11 +35,22 @@ public class Data {
         }
     }
 
+    /**
+     *
+     *     This function takes in a value and the number of decimal places as input and returns the rounded number upto the given decimal places.
+     */
 
     private double round(double value, int decimalPlaces) {
         double scale = Math.pow(10, decimalPlaces);
         return Math.round(value * scale) / scale;
     }
+
+    /**
+     *
+     * This function is responsible for performing all statistical operations on the given data.
+     * It takes in the required parameters and the function name which needs to be operated on the given data.
+     * The output of this operation is returned to the calling function.
+     */
 
     public Map<String, String> stats(int decimalPlaces, List<ColumnData> showCols, String functionName) {
         Map<String, String> stats = new HashMap<>();
